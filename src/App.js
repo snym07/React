7cb91d6432ee77739a4e86352f2b49562958933e
Sanyam <snym07@Sanyams-MacBook-Air.local>
2016-07-23 20:18:02 +0530

    adds node modules

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
 class App extends Component {
 	render() {
 		return (
 			<div>
 				<h1>React</h1>
+				<MuiThemeProvider>
+				 <RaisedButton label="Default" />
+				</MuiThemeProvider>
 			</div>
 		);
 	}
